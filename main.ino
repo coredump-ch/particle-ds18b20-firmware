7db186211cc187406afacb7159b52b76cb2228ee
Danilo Bargen <mail@dbrgn.ch>
2016-01-12 11:22:51 +0100

    More source code comments

--- main.ino
+++ main.ino
@@ -92,19 +92,35 @@ void loop(void) {
     Serial.print(OneWire::crc8(data, 8), HEX);
     Serial.println();
 
+    // Format of the data (page 7 of datasheet):
+    //
+    //  data[0] -> Temperature LSB
+    //  data[1] -> Temperature MSB
+    //  data[2] -> T_H register or user byte 1
+    //  data[3] -> T_L register or user byte 2
+    //  data[4] -> Configuration register
+    //  data[5] -> Reserved (FFh)
+    //  data[6] -> Reserved (0Ch)
+    //  data[7] -> Reserved (10h)
+    //  data[8] -> CRC
+
     // Convert the data to actual temperature.
     // Because the result is a 16 bit signed integer, it should be stored to an
     // "int16_t" type, which is always 16 bits even when compiled on a 32 bit
     // processor.
     int16_t raw = (data[1] << 8) | data[0];
     byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) {
-        raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    } else if (cfg == 0x20) {
-        raw = raw & ~3; // 10 bit res, 187.5 ms
-    } else if (cfg == 0x40) {
-        raw = raw & ~1; // 11 bit res, 375 ms
+
+    // At lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) { // 9 bit resolution, 93.75 ms conversion time
+        // Clear the lowest 3 bits
+        raw = raw & ~7;
+    } else if (cfg == 0x20) { // 10 bit res, 187.5 ms conversion time
+        // Clear the lowest 2 bits
+        raw = raw & ~3;
+    } else if (cfg == 0x40) { // 11 bit res, 375 ms conversion time
+        // Clear the lowest bit
+        raw = raw & ~1;
     }
     // default is 12 bit resolution, 750 ms conversion time
 
